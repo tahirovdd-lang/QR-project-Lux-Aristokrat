90f9b2429871b081994082be6e5cc86339180e9f
tahirovdd-lang <tahirov.dd@gmail.com>
2026-04-04 12:43:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,19 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.filters import Command
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-from config import BOT_TOKEN
-from services import get_or_create_user
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден в переменных окружения")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Сканировать QR")],
-        [KeyboardButton(text="Мои баллы")]
-    ],
-    resize_keyboard=True
-)
-
-@dp.message(Command("start"))
-async def start(message: Message):
-    user_id = message.from_user.id
-    get_or_create_user(user_id)
-
-    await message.answer(
-        "Добро пожаловать 👋\n"
-        "Нажми 'Сканировать QR' чтобы получить баллы",
-        reply_markup=kb
-    )
-
-@dp.message(F.text == "Мои баллы")
-async def points(message: Message):
-    user_id = message.from_user.id
-    points = get_or_create_user(user_id)
-
-    await message.answer(f"Ваш баланс: {points} баллов")
-
-@dp.message(F.text == "Сканировать QR")
-async def scan(message: Message):
-    await message.answer("⚠️ Сканер подключим на следующем этапе")
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Бот работает ✅")
 
 async def main():
     await dp.start_polling(bot)
